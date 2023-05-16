d53d039370f46745a6ffca08d281b82d618b24cc
leonardobonato <leoleobonaato@gmail.com>
2023-05-16 16:09:55 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function receitas(){
+
+    window.location.href = "https://www.recepedia.com/pt-br/receita/";
+}
+
+function achar(){
+
+    window.location.href = "https://www.google.com/search?q=sites+de+receitas&sxsrf=APwXEdf34CcRTpmz1bH_NJB4mZpxkc5k8A%3A1684259670218&ei=VsNjZPP2DPHA5OUPtOuvoA4&ved=0ahUKEwjz-eiatPr-AhVxILkGHbT1C-QQ4dUDCA8&uact=5&oq=sites+de+receitas&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIHCAAQigUQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoHCCMQigUQJzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwgAEIAEELEDEIMBOgsILhCABBCxAxCDAToLCAAQigUQsQMQgwE6CAgAEIAEELEDOgsIABCABBCxAxDJAzoICAAQigUQkgM6CAgAEIAEEJIDSgQIQRgAUABY_BJgqBNoAHABeACAAaoBiAGJEZIBBDAuMTaYAQCgAQHAAQE&sclient=gws-wiz-serp"
+}
+
+function livros(){
+
+    window.location.href = "https://www.amazon.com.br/s?k=livros+de+culinaria&i=stripbooks&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2QTDDR3LO889F&sprefix=livros+de+culin%C3%A1r%2Cstripbooks%2C496&ref=nb_sb_noss_2"
+}
